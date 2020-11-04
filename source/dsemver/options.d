a7ee053e6c6e19ed6a9ab106d33f87a834bbf2cd
Robert burner Schadek <robert.schadek@informatik.uni-oldenburg.de>
2020-11-04 10:13:28 +0100

    Fixes #1

--- source/dsemver/options.d
+++ source/dsemver/options.d
@@ -39,8 +39,27 @@ ref Options getWritableOptions() {
 void getOptOptions(ref string[] args) {
 	import core.stdc.stdlib : exit;
 	bool helpWanted = parseArgsWithConfigFile(getWritableOptions(), args);
-	if(helpWanted) {
-		printArgsHelp(getOptions(), "A text explaining the program");
+	if(helpWanted || args.length == 1) {
+		printArgsHelp(getOptions(),
+`
+
+dsemver lets the computer compute the SemVer of your dlang software.
+
+If this is the first time running dsemver on your project you like want to
+run
+
+'''
+$ ./dsemver -- -p PATH_TO_PROJECT -l
+'''
+
+To compute the public interface of your latest git tag that looked it a SemVer
+than you can run
+
+'''
+$ ./dsemver -- -p PATH_TO_PROJECT -c
+'''
+
+To compute the next SemVer of your project.`);
 		exit(0);
 	}
 }
